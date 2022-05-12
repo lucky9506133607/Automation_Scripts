08c8fe23b9fb0d2a652afe4303211e829defc18e
lucky9506133607 <ls2170184@gmail.com>
2022-05-12 11:43:27 +0530

    12-05-2022 changes

--- NotExistfile.py
+++ NotExistfile.py
@@ -17,20 +17,24 @@ sleep(5)
 alert_obj = driver.switch_to.alert
 print("Alert shows following message", alert_obj.text)
 alert_obj.dismiss()
+filename = input("Please! Enter your filename = ")
 
-path = '/home/nteam/Desktop/first.png'
+path = '/home/nteam/Desktop/Automation Script Github/'+filename+'.png'
 isExist = os.path.exists(path)
 print(isExist)
 
 if isExist:
+    print("File is already exist")
     newfilename = input("Enter new file name = ")
     driver.save_screenshot(newfilename+'.png')
     image = Image.open(newfilename+'.png')
     image.show()
 
 else:
-    filename = re.findall("([^\/]+\.png)", path)
-    driver.save_screenshot(filename[0])
+    print("File not exist")
+    #filename = re.findall("([^\/]+\.png)", path)
+    driver.save_screenshot(filename+'.png')
     image = Image.open(filename[0])
     image.show()
+    
 
